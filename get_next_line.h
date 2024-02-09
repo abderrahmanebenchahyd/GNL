a74d5b5fb7cdb8688a91be3201802f591b57601d
abbencha <abderrahmanebenchahyd856@gmail.com>
2024-02-09 19:44:35 +0100

    header

--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,10 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
